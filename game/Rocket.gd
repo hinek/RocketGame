a6919b19767b49b71de9b8ba19cc16a94afb4fd4
derHinek <jan-hendrik.roehrs@bauermedia.com>
2020-12-11 21:46:04 +0100

    give 2 seconds invincibility after rocket took a hit

--- game/Rocket.gd
+++ game/Rocket.gd
@@ -5,6 +5,8 @@ const EXPLOSION_TYPE = preload("res://Explosion.tscn")
 const MOVEMENT_SPEED = 250
 
 
+var invincibility = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,12 +20,18 @@ func _process(delta):
 	rotation = target_angle
 	var movement = target_angle if target_angle < PI else -(2*PI-target_angle)
 	position.y = clamp(position.y + movement * delta * MOVEMENT_SPEED, 30, 570)
+	
+	if invincibility > 0:
+		invincibility -= delta
+		$Sprite.modulate.a = 1.0 if int(invincibility * 30) % 2 == 0 else 0.2
 
 
 
 func _on_Rocket_area_entered(area):
-	get_parent().call_deferred("rocket_damaged")
-	var explosion = EXPLOSION_TYPE.instance()
-	explosion.position = area.position
-	get_parent().add_child(explosion)
-	area.queue_free()
+	if invincibility <= 0:
+		get_parent().call_deferred("rocket_damaged")
+		var explosion = EXPLOSION_TYPE.instance()
+		explosion.position = area.position
+		get_parent().add_child(explosion)
+		area.queue_free()
+		invincibility = 2
